e77cfdeedad63a9e2322bc886a039fa752ab78a9
I-am-aman <readyaman@gmail.com>
2019-12-12 23:47:25 +0530

    First Commit

---
+++ app/build/generated/source/buildConfig/debug/one_night_hack/hack36/driverscompanion/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package one_night_hack.hack36.driverscompanion;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "one_night_hack.hack36.driverscompanion";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
